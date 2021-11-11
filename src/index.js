788a222a7cb28a261e18b603dcacf0bd179a8f4a
ylgr <quyen.luminous@gmail.com>
2021-11-11 17:16:30 +0700

    (!) Disable router

--- src/index.js
+++ src/index.js
@@ -17,21 +17,10 @@
 */
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 
 // core components
-import Admin from "layouts/Admin.js";
-import RTL from "layouts/RTL.js";
 
 import "assets/css/material-dashboard-react.css?v=1.10.0";
+import UserProfile from "./views/UserProfile/UserProfile";
 
-ReactDOM.render(
-  <BrowserRouter>
-    <Switch>
-      <Route path="/admin" component={Admin} />
-      <Route path="/rtl" component={RTL} />
-      <Redirect from="/" to="/admin/dashboard" />
-    </Switch>
-  </BrowserRouter>,
-  document.getElementById("root")
-);
+ReactDOM.render(<UserProfile />, document.getElementById("root"));
